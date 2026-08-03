8881feb6e146e6e12d4e3e726bc64562e7f74667
Mert <mertisik329@gmail.com>
2026-08-03 02:15:40 +0000

    finished r3 and r4

--- include/limen/limen_devinfo.h
+++ include/limen/limen_devinfo.h
@@ -16,7 +16,21 @@ void parse_argv(int argc, char* argv[],parsed_args* args_container);
 // 0-> successful
 // 2-> no devices registered
 // 3-> verbs error 
-int enumerate_devices();
+// print_to_err =1-> print to std::cerr
+int enumerate_devices(ibv_device** devices_list, int print_to_err);
+
+//  returns the index of the first ibv_device whose name 
+//  matches device_name; returns -1 if not found
+int find_device_by_name(ibv_device** devices_list, const char* device_name);
+
+void print_device_info(ibv_device_attr* attr);
+
+void print_port_info(ibv_port_attr* attr);
+
+void allocate_protection_domain();
+
+void register_memregion();
+
 
 
 
--- src/limen_devinfo.cpp
+++ src/limen_devinfo.cpp
@@ -3,8 +3,11 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <inttypes.h>
 #include <infiniband/verbs.h>
+#include <endian.h>
 #include "limen/limen_devinfo.h"
+
 
 void parse_argv(int argc, char* argv[],parsed_args* args_container)
 {
@@ -67,27 +70,26 @@ void parse_argv(int argc, char* argv[],parsed_args* args_container)
     }
 }
 
-int enumerate_devices()
+int enumerate_devices(ibv_device** devices_list,int print_to_err)
 {
-    //  open device list
-    ibv_device ** device_list = ibv_get_device_list(NULL);
-    //  enumerate devices (if no args given)
-    //  check if the call failed (exit code 3)
-    if 
-    (device_list == NULL)
-    {
-        perror("ibv_get_device_list");
-        return 3;
-    }
+
     int device_idx=0;
     ibv_device* device;
     while 
-    ((device = device_list[device_idx])!=NULL)
+    ((device = devices_list[device_idx])!=NULL)
     {
         // need to get device name and guid for each device
         const char* name = ibv_get_device_name(device);
-        uint64_t guid = ibv_get_device_guid(device);
-        printf("device: %s guid: 0x%lx\n",name,guid);
+        uint64_t guid = be64toh(ibv_get_device_guid(device));
+        if
+        (print_to_err)
+        {
+            fprintf(stderr,"device: %s guid: 0x%016" PRIx64 "\n",name,guid);
+        }
+        else
+        {
+            printf("device: %s guid: 0x%016" PRIx64 "\n",name,guid);
+        }
         device_idx++;
     }
 
@@ -98,37 +100,147 @@ int enumerate_devices()
         std::cerr << "No RDMA devices registered with kernel\n";
         return 2;
     }
-    
-    printf("devices: %i\n",device_idx);
-    ibv_free_device_list(device_list);
+    if
+    (print_to_err)
+    {
+        fprintf(stderr,"devices: %i\n",device_idx);
+    }
+    else
+    {
+        printf("devices: %i\n",device_idx);
+    }
     return 0;
+}
+
+int find_device_by_name(ibv_device** devices_list, const char* device_name)
+{
+    int device_idx=0;
+    ibv_device* device;
+    while 
+    ((device = devices_list[device_idx])!=NULL)
+    {
+        // need to get device name and guid for each device
+        const char* name = ibv_get_device_name(device);
+        if 
+        (strcmp(name,device_name) == 0)
+        {
+            return device_idx;
+        }
+        device_idx++;
+    }
+    return -1;
+}
+
+void print_device_info(ibv_device_attr* attr)
+{
+
+
+    // print out all the data here:
+    const char* fmt64 = "\t%s: %llu\n";
+    const char* fmtint = "\t%s: %i\n";
+    uint64_t guid = be64toh(attr->node_guid);
+
+    printf("\t%s: 0x%016" PRIx64 "\n","guid",guid);
+    printf("\tfw_ver: %s\n",attr->fw_ver);
+    printf(fmtint,"phys_port_cnt",attr->phys_port_cnt);
+    printf(fmtint,"max_qp",attr->max_qp);
+    printf(fmtint,"max_qp_wr",attr->max_qp_wr);
+    printf(fmtint,"max_cq",attr->max_cq);
+    printf(fmtint,"max_cqe",attr->max_cqe);
+    printf(fmtint,"max_mr",attr->max_mr);
+    printf(fmt64,"max_mr_size",attr->max_mr_size);
+    printf(fmtint,"max_sge",attr->max_sge);
+    printf(fmtint,"max_qp_rd_atom",attr->max_qp_rd_atom);
+}
+
+void print_port_info(ibv_port_attr* attr)
+{
+    printf("\tstate: ")
 }
 
 int main(int argc, char* argv[])
 {
+    //  VARIABLES
+    ibv_device** device_list;
+    ibv_device* device;
+    ibv_context* device_context;
+    ibv_device_attr device_attr;
+    ibv_port_attr port_attr;
+
     //  handle arguments
     parsed_args args_container;
     args_container.port = 1; // default port num
     parse_argv(argc,argv,&args_container);
 
+    //  open device list
+    device_list = ibv_get_device_list(NULL);
+    if 
+    (device_list == NULL)
+    {
+        perror("ibv_get_device_list");
+        //  TODO graceful exit
+        exit(3);
+    }
 
     if
     (argc ==1)
     {
-        enumerate_devices();
+        //  enumerate mode
+        int rc = enumerate_devices(device_list,0);
+        ibv_free_device_list(device_list);
+        //  TODO graceful exit
+        exit(rc);
     }
 
     if 
     (args_container.device_name != nullptr)
     {
         //  (-d) open a named device context
-
-
-         //  query device & print fields
-
+        int device_idx = find_device_by_name(device_list,args_container.device_name);
+        if
+        (device_idx == -1)
+        {
+            // not found, enumerate & exit 2
+            enumerate_devices(device_list,1);
+            ibv_free_device_list(device_list);
+            //  TODO graceful exit
+            exit(2);
+        }
+        //  query device & print fields
+        device = device_list[device_idx];
+        //  open device to get ibv_context* ptr
+        device_context = ibv_open_device(device);
+        printf("device: %s\n",args_container.device_name);
+        if
+        (ibv_query_device(device_context,&device_attr) != 0)
+        {
+            perror("ibv_query_device");
+            //  TODO graceful exit
+            exit(3);
+        }
+        print_device_info(&device_attr);
     }
 
     //  query port & print fields
+    if
+    (args_container.port > device_attr.phys_port_cnt)
+    {
+        fprintf(stderr,"port number cannot be greater than phys_port_cnt\n");
+        //  TODO graceful exit
+        exit(1);
+    }
+
+    if
+    (ibv_query_port(device_context,args_container.port,&port_attr) != 0)
+    {
+        //  TODO GRACEFUL EXIT
+        perror("ibv_query_port");
+        exit(3);
+    }
+
+    printf("port: %i\n",args_container.port);
+    print_port_info(&port_attr);
+    
 
     //  allocate protection domain on context 
 
@@ -149,7 +261,6 @@ int main(int argc, char* argv[])
 
 
 
-
-    printf("hello world\n");
+    ibv_free_device_list(device_list);
     return 0;
 }
